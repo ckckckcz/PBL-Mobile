787bbce16b7ec827708c4c05b737323ce8039c02
dinaarh <rahmawatidina0404@gmail.com>
2025-12-08 06:02:00 +0700

    update

--- frontend/lib/pages/edit_profile_page.dart
+++ frontend/lib/pages/edit_profile_page.dart
@@ -1,4 +1,3 @@
-// FILE: edit_profile_page.dart
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
--- frontend/lib/pages/profile_page.dart
+++ frontend/lib/pages/profile_page.dart
@@ -1,4 +1,3 @@
-// FILE: profile_page.dart
 import 'package:flutter/material.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'about_app.dart';
